9f676f7dbcb82441e38b3c9d006513b2c2f0489b
tomyoll <slavamazur21@gmail.com>
2022-02-05 23:03:13 +0200

    clear utils store

--- src/store/UtilsStore.js
+++ src/store/UtilsStore.js
@@ -1,14 +1,3 @@
 export default class UtilsStore {
-  constructor() {
-    this._gridMode = localStorage.getItem('gridMode') || 'standard';
-  }
-
-  setGridMode(mode) {
-    this._gridMode = mode;
-    localStorage.setItem('gridMode', mode);
-  }
-
-  get gridMode() {
-    return localStorage.getItem('gridMode');
-  }
+  constructor() { }
 }
